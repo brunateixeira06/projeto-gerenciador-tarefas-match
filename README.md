74149e592db6969f963e40e890d5f23d729268b8
brunateixeira06 <brunateixeira.silvabdst@gmail.com>
2024-01-15 13:27:37 -0300

    adicionando

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gerenciador de Tarefas com Verificação de Prioridade
+
+## Projeto desenvolvido para o módulo II do Match! IBM + Mastertech.
+
+Neste projeto, foi criado um aplicativo de gerenciamento de tarefas em Python.
+
+A aplicação incluirá as seguintes etapas:
+
+➔ Cadastro de Tarefas:
+
+◆ Os usuários poderão cadastrar tarefas, incluindo descrição e data de vencimento. Eles também atribuirão uma prioridade a cada tarefa.
+
+➔ Validação de Prioridade:
+
+◆ A aplicação verificará se a prioridade atribuída a cada tarefa está dentro de um intervalo válido.
+
+➔ Gerenciamento de Tarefas:
+
+◆ Os usuários poderão visualizar, atualizar e excluir tarefas.
+
+◆ A aplicação exibirá as tarefas de acordo com suas prioridades.
---
+++ api/main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tarefas = []
+
+class Tarefa:
+    def __init__(self, id, descricao, data, prioridade, concluida) -> None:
+        self.id = id
+        self.descricao = descricao
+        self.data = data
+        self.prioridade = prioridade
+        self.concluida = concluida
+
+
+
+prioridade_para_valor = {"Urgente": 1, "Importante": 2, "Não urgente": 3}
+
+
+@app.route("/")
+def index():
+    tarefas_ordenadas = sorted(tarefas, key=lambda x: (prioridade_para_valor[x.prioridade], x.descricao))
+    return render_template("index.html", tarefas=tarefas_ordenadas)
+
+
+@app.route("/adicionar", methods=['POST'])
+def adicionar_tarefa():
+    global tarefas
+    tarefa = Tarefa(len(tarefas) + 1, request.form['descricao'],  request.form['data'],  request.form['prioridade'], False)
+    
+    tarefas.append(tarefa)
+    return redirect("/")
+
+
+@app.route('/excluir/<int:tarefa_id>', methods=['DELETE'])
+def excluir_tarefa(tarefa_id):
+    global tarefas
+    for tarefa in tarefas:
+        if tarefa.id == tarefa_id:
+            tarefas.remove(tarefa)
+
+    return '', 200
+
+
+@app.route('/concluir/<int:tarefa_id>', methods=['PUT'])
+def concluir_tarefa(tarefa_id):
+    global tarefas
+    tarefa = next((t for t in tarefas if t.id == tarefa_id), None)
+    tarefa.concluida= True
+    
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
