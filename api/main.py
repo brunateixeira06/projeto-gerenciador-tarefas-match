00bfde2f921812706deeb35d644cae4bf092a286
brunateixeira06 <brunateixeira.silvabdst@gmail.com>
2024-01-15 22:11:19 -0300

    atualizado

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 tarefas = []
 
-class Tarefa:c
+class Tarefa:
     def __init__(self, id, descricao, data, prioridade, concluida) -> None:
         self.id = id
         self.descricao = descricao
